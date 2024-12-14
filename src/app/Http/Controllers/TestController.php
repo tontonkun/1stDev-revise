0aec10df826569f725b5fc62908c6be616b91ac5
fujii daigo <daigofujii1116@gmail.com>
2024-12-14 19:54:07 +0900

    build_1214

---
+++ src/app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $item = [
+            'content' => 'パラメータを渡す',
+        ];
+        return view('index', $item);
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,9 @@
+コピーしました！
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\HelloController;
+
+Route::get('/test', [TestController::class, 'index']);
+Route::get('/hello', [HelloController::class, 'index']);
